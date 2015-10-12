bc587cb6a1a858b4a9242833262edcea08c512fe
Ciao Chow <ciaochow15@zoho.com>
2015-10-12 17:03:42 -0400

    Create Initial Console App

---
+++ ADOpresentationdemo/ADODemoApp/Config/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ADODemoApp.Config
+{
+    public class Settings
+    {
+        public static string ConnectionString = ConfigurationManager.ConnectionStrings["Northwind"].ConnectionString;
+    }
+}
---
+++ ADOpresentationdemo/ADOpresentationdemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADODemoApp.Repo;
+using ADODemoApp.Model;
+
+namespace ADOpresentationdemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintOrders();
+            Console.ReadLine();
+        }
+
+        private static void PrintOrders()
+        {
+            OrderRepo oR = new OrderRepo();
+            List<Order> Orders = oR.GetAll();
+            foreach (var or in Orders)
+            {
+                Console.WriteLine("-------------------");
+                Console.WriteLine(or.OrderID);
+                Console.WriteLine(or.OrderDate);
+                Console.WriteLine(or.RequiredDate);
+                Console.WriteLine(or.ShippedDate);
+                Console.WriteLine(or.ShipName);
+                Console.WriteLine(or.ShipAddress);
+                Console.WriteLine(or.ShipCity);
+                Console.WriteLine(or.ShipRegion);
+                Console.WriteLine(or.ShipPostalCode);
+                Console.WriteLine(or.ShipCountry);
+            }
+        }
+    }
+}
