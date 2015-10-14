3c88efe1cbbd827473274884e6aaf7c92e657c30
unknown <forthofer5@hotmail.com>
2015-10-13 21:56:51 -0400

    created simple mvc web ui

--- ADOpresentationdemo/ADODemoApp.Web/Controllers/HomeController.cs
+++ ADOpresentationdemo/ADODemoApp.Web/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ADODemoApp.Model;
+using ADODemoApp.Repo;
+using ADODemoApp.Web.Models;
 
 namespace ADODemoApp.Web.Controllers
 {
@@ -13,18 +16,28 @@ namespace ADODemoApp.Web.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Top5Products()
         {
-            ViewBag.Message = "Your application description page.";
+            AW_ProductRepo aw_pR = new AW_ProductRepo();
+            List<AW_Product> list = aw_pR.GetAll();
+            return View(list);
+        }
 
+        public ActionResult GetProductsByGender()
+        {
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult ListProductsByGender(string choice)
         {
-            ViewBag.Message = "Your contact page.";
+            AW_ProductModelRepo aw_pmR = new AW_ProductModelRepo();
+            aw_pmR.CreateGetClothingProcedureIfNull();
 
-            return View();
+            if (choice == "")
+                return View("Error");
+
+            List<AW_ProductModel> list = aw_pmR.GetProductsByGender(choice);
+            return View(list);
         }
     }
 }
